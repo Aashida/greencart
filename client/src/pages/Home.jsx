cede9554fc4f5191d329190e5ac255a1f40395f8
Aashida S <aash22002.it@rmkec.ac.in>
2025-06-11 16:16:55 +0530

    greencart

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import BestSeller from '../components/BestSeller'
+import Categories from '../components/Categories'
+import MainBanner from '../components/MainBanner'
+
+const Home = () => {
+  return (
+    <div className='mt-10'>
+      <MainBanner />
+      <Categories/>
+      <BestSeller/>
+    </div>
+  )
+}
+
+export default Home
